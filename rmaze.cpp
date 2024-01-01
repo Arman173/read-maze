4791e35c828fc9f16d87037a1de7fc61617583aa
Arman173 <armanhaz173@gmail.com>
2023-12-31 19:21:38 -0600

    set start/end point finished

--- rmaze.cpp
+++ rmaze.cpp
@@ -1,12 +1,35 @@
 #include "rmaze.h"
 
+vector_m::vector_m()
+{
+    this->x = 0;
+    this->y = 0;
+}
+
+vector_m::vector_m(unsigned int x, unsigned int y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+std::ostream &operator<<(std::ostream &os, const vector_m vector)
+{
+    os << "x: " << vector.x << ", y: " << vector.y;
+    return os;
+}
+
 Maze::Maze(/* args */)
 {
     this->m_mat = NULL;
+    this->ROWS = 0;
+    this->COLUMNS = 0;
 }
 
 Maze::~Maze()
 {
+    std::cout << std::endl
+              << "deleting: " << this->ROWS * this->COLUMNS;
+    std::cout << " elements..." << std::endl;
     delete[] this->m_mat;
 }
 
@@ -21,29 +44,86 @@ Maze::Maze(const char *filename)
     }
     // contamos cuantas columnas hay
     char c;
-    int columns = 0;
+    // int columns = 0;
     while (file.get(c))
         if (c == '\n')
             break;
-    columns = (int)file.tellg() - 1;
-    std::cout << std::endl
-              << "gcount: " << file.gcount() << std::endl;
-    std::cout << std::endl
-              << "columns: " << columns << std::endl;
-    std::cout << std::endl
-              << "tellg: " << file.tellg() << std::endl;
+    this->COLUMNS = (int)file.tellg() - 1;
+    // volvemos al inicio del archivo
     file.seekg(0, file.end);
     int lenght = file.tellg();
     file.seekg(0, file.beg);
-    int rows = 0;
+    // contamos las filas
+    this->ROWS = 0;
     while (file.tellg() < lenght)
     {
-        file.seekg(columns + 1, file.cur);
-        rows++;
-        std::cout << file.tellg() << std::endl;
+        file.seekg(this->COLUMNS + 1, file.cur);
+        this->ROWS++;
+        // std::cout << file.tellg() << std::endl;
     }
+    // reservamos memoria...
+    this->m_mat = new char[this->ROWS * this->COLUMNS];
     std::cout << std::endl
-              << "rows: " << rows << std::endl;
+              << "maze size: " << sizeof(char) * ROWS * COLUMNS << std::endl;
+    // volvemos al inicio del archivo
+    file.seekg(0, file.beg);
+    // guardamos el laberinto en nuestro arreglo
+    unsigned int i = 0;
+    while (file.get(c) && i < this->ROWS * this->COLUMNS)
+    {
+        // std::cout << c;
+        if (c == '\n')
+        {
+            std::cout << std::endl;
+            continue;
+        }
+        std::cout << c;
+        this->m_mat[i] = (c == '*') ? 1 : 0;
+        if (c == 'I')
+        {
+            _start = vector_m(i / this->COLUMNS, i % this->COLUMNS);
+            // std::cout << std::endl
+            //           << _start << std::endl;
+        }
+        if (c == 'F')
+        {
+            _end = vector_m(i / this->COLUMNS, i % this->COLUMNS);
+            // std::cout << std::endl
+            //           << _end << std::endl;
+        }
+        i++;
+    }
+    std::cout << std::endl;
+    // for (int i = 0; i < this->ROWS * this->COLUMNS; i++)
+    //     std::cout << this->m_mat[i];
+    // std::cout << std::endl;
     // cerramos el archivo
     file.close();
+}
+
+char &Maze::operator()(unsigned int r, unsigned int c)
+{
+    if ((r < 0 || c < 0) || (r >= this->ROWS || c >= this->COLUMNS))
+    {
+        exit(EXIT_FAILURE);
+    }
+    return this->m_mat[r * this->COLUMNS + c];
+}
+
+unsigned int Maze::rows()
+{
+    return this->ROWS;
+}
+unsigned int Maze::columns()
+{
+    return this->COLUMNS;
+}
+
+vector_m Maze::beg()
+{
+    return this->_start;
+}
+vector_m Maze::end()
+{
+    return this->_end;
 }
--- rmaze.h
+++ rmaze.h
@@ -6,15 +6,38 @@
 #include <fstream>
 #include <cstring>
 
+struct vector_m
+{
+    unsigned int x;
+    unsigned int y;
+
+public:
+    vector_m();
+    vector_m(unsigned int, unsigned int);
+    friend std::ostream &operator<<(std::ostream &, const vector_m);
+};
+
 class Maze
 {
 private:
     /* data */
-    int *m_mat; // maze matrix
+    unsigned int ROWS;
+    unsigned int COLUMNS;
+    char *m_mat; // maze matrix
+    vector_m _start;
+    vector_m _end;
+
 public:
     Maze(/* args */);
     Maze(const char *);
     ~Maze(); // destructor
+    /*operators*/
+    char &operator()(unsigned int, unsigned int);
+    /*getters*/
+    unsigned int rows();
+    unsigned int columns();
+    vector_m beg();
+    vector_m end();
 };
 
 #endif
